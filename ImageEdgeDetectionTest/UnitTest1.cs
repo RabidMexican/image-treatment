956ccd63d945cf463efe5b2bd927484c4dc36ded
RabidMexican <rabidmexican@gmail.com>
2020-11-20 10:10:35 +0100

    init of test project
    Created a new unit test project in the solution and  created the reference to be able to call methods from main project

---
+++ ImageEdgeDetectionTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageEdgeDetection;
+
+namespace ImageEdgeDetectionTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            
+            //ExtBitmap.ApplyFilterSwap();
+        }
+    }
+}
