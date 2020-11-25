214c68ca12f0a63df4e492153e3b1affe3cb94ca
RabidMexican <rabidmexican@gmail.com>
2020-11-25 09:31:03 +0100

    Tidied code and created testfunc class
    Tidied a bit and added a class to contain all of our test functions, this way we only have tests in the test classes

---
+++ ImageEdgeDetectionTest/TestFunctions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography;
+
+namespace ImageEdgeDetectionTest
+{
+    class Test
+    {
+        public static string GetImageHash(Bitmap bmpSource)
+        {
+            List<byte> colorList = new List<byte>();
+            string hash;
+
+            int i, j;
+            Bitmap bmpMin = new Bitmap(bmpSource, new Size(16, 16)); //create new image with 16x16 pixel
+            for (j = 0; j < bmpMin.Height; j++)
+            {
+                for (i = 0; i < bmpMin.Width; i++)
+                {
+                    colorList.Add(bmpMin.GetPixel(i, j).R);
+                }
+            }
+
+            SHA1Managed sha = new SHA1Managed();
+            byte[] checksum = sha.ComputeHash(colorList.ToArray());
+            hash = BitConverter.ToString(checksum).Replace("-", String.Empty);
+
+            sha.Dispose();
+            bmpMin.Dispose();
+
+            return hash;
+        }
+    }
+}
--- ImageEdgeDetectionTest/UnitTest1.cs
+++ ImageEdgeDetectionTest/UnitTest1.cs
@@ -5,9 +5,6 @@ using System.IO;
 using System;
 using System.Collections.Generic;
 using System.Security.Cryptography;
-using System.Reflection;
-using System.Resources;
-using ImageEdgeDetectionTest.Properties;
 
 namespace ImageEdgeDetectionTest
 {
@@ -17,61 +14,29 @@ namespace ImageEdgeDetectionTest
         [TestMethod]
         public void TestSwapMethod()
         {
-
-
             //get images from Resources
-            Bitmap testImage = ImageEdgeDetectionTest.Properties.Resources.bag;
-            Bitmap realResult = ImageEdgeDetectionTest.Properties.Resources.bag_swap;
+            Bitmap testImage = Properties.Resources.original;
+            Bitmap realResult = Properties.Resources.original_swap;
 
             //apply filter on test image
             Bitmap result = ExtBitmap.ApplyFilterSwap(testImage);
 
-
-
             //get Hash from images
-            String resultImageHash = GetImageHash(result);
-            String realResultImageHash = GetImageHash(realResult);
-
+            string resultImageHash = TestFunctions.GetImageHash(result);
+            string realResultImageHash = TestFunctions.GetImageHash(realResult);
 
             //comparison
             Assert.AreEqual(resultImageHash, realResultImageHash);
-           
+
         }
 
-        public Bitmap getTestImage(String imageName)
+        public Bitmap getTestImage(string imageName)
         {
-         
             StreamReader streamReader = new StreamReader(imageName);
             Bitmap image = (Bitmap)Image.FromStream(streamReader.BaseStream);
             streamReader.Close();
 
-         return image;
+            return image;
         }
-
-
-        private string GetImageHash(Bitmap bmpSource)
-        {
-         List<byte> colorList = new List<byte>();
-         string hash;
-            
-           colorList.Clear();
-            int i, j;
-            Bitmap bmpMin = new Bitmap(bmpSource, new Size(16, 16)); //create new image with 16x16 pixel
-            for (j = 0; j < bmpMin.Height; j++)
-            {
-                for (i = 0; i < bmpMin.Width; i++)
-                {
-                    colorList.Add(bmpMin.GetPixel(i, j).R);
-                }
-            }
-            SHA1Managed sha = new SHA1Managed();
-            byte[] checksum = sha.ComputeHash(colorList.ToArray());
-            hash = BitConverter.ToString(checksum).Replace("-", String.Empty);
-            sha.Dispose();
-            bmpMin.Dispose();
-            return hash;
-        }
-
-
     }
 }
